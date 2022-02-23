1256277f1505c32bf2d10734c610800e7446d1cd
Rafał Szczepaniak <szczepaniak93@gmail.com>
2022-02-23 23:56:24 +0100

    Poprawa drzewa

--- src/com/home/Main.java
+++ src/com/home/Main.java
@@ -1,8 +1,21 @@
 package com.home;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Podaj swoje imię: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+
+        System.out.println("Podaj swój wiek: ");
+        int wiek = scanner.nextInt();
+        System.out.println("Podaj nazwisko");
+        scanner.nextLine();
+        String nazwisko = scanner.nextLine();
+
+        System.out.println(String.format("Witaj %s %s mający lat %s", a, nazwisko, wiek));
     }
 }
